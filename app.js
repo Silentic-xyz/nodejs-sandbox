3339b877ad3ba2209c24b2d285f1984164101a46
Artemusss <artem129871@gmail.com>
2020-06-10 13:56:39 +0300

    Modified app.js

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const app = express();
 app.use(bp.json());
 
 app.post('/', (req, res) => {
-  let code = req.body.code;
-  let output = new VM().run(code);
-  res.send(output);  
+  let code = req.body['code'];
+  console.log(req.body);
+  let vm = new VM();
+  let out = vm.run(code);
+  res.send(out);
 });
 
-app.listen(3000);
+app.listen(3002);
